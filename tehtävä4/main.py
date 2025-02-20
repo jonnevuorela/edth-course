e2937be11346187649a3a715359e08917232bab9
jonne <jonne.vuorela@proton.me>
2025-02-20 10:08:28 +0200

    puutteellisen datan korjaus

--- tehtävä4/main.py
+++ tehtävä4/main.py
@@ -12,13 +12,13 @@ def load_csv():
         employee_list: list[Employee] = []
         discarded: list[str] = []
 
-        # Regex pattern AI genereoitu. Claude 3.5 Sonnet
+        # Regex pattern AI genereoitu, josta muokattu sopivaksi. Claude 3.5 Sonnet
         # Create me a regex pattern that matches the context.
         pattern: re.Pattern = re.compile(
-            r'(\d+),(\w+),([^,]+),([^,]+),(\d+),(\d+(?:\.\d+)?)')
+            r'(\d+),([^,]+),([^,]+),([^,]+),[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+),[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)')
 
         for line in file:
-            match: bool = pattern.match(line.rstrip())
+            match = pattern.match(line.rstrip())
             if match:
                 fields: list[str] = line.rstrip().split(',')
                 employee = Employee(
@@ -32,10 +32,33 @@ def load_csv():
                 employee_list.append(employee)
             else:
                 discarded.append(line)
-                print("no match")
-        printlist(employee_list)
-        print("------discarded-------")
-        printlist(discarded)
+
+        incomplete_employees = parseMissingData(discarded)
+        for incomplete_employee in incomplete_employees:
+            employee_list.append(incomplete_employee)
+
+
+def parseMissingData(data: list[str]):
+
+    employee_list: list[Employee] = []
+
+    # csv:n ensimmäisen rivin (tietotyyppi rivi) hylkäämiseen voisi olla parempiakin ratkaisuja
+    for line in data[1:]:
+        fields: list[str] = line.rstrip().split(',')
+        employee = Employee(
+            age=int(fields[0]) if fields[0].isdigit() else 0,
+            gender=str(fields[1]) or 'Other',
+            education_level=str(fields[2]) or 'no education',
+            job_title=str(fields[3]) or 'no title',
+            years_of_experience=float(
+                fields[4]) if fields[4].isdigit() else 0,
+            salary=float(fields[5]) if fields[5].isdigit() else 0,
+        )
+
+        if employee.age != 0:  # hylkää haamurivit.
+            employee_list.append(employee)
+
+    return employee_list
 
 
 load_csv()
